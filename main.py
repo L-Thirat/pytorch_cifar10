9b33f3084205f18b638cad80d9d5e8de464430ae
TerusLimsurut <few2012few@gmail.com>
2019-08-07 14:55:37 +0900

    .

--- main.py
+++ main.py
@@ -21,20 +21,20 @@ args = parser.parse_args()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-end_epoch=200
+end_epoch=5
 
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
+    # transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform_train)
@@ -48,8 +48,8 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship'
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
-# net = ResNet18()
-net = PreActResNet18()
+net = ResNet18()
+# net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
 # net = ResNeXt29_2x64d()
@@ -80,18 +80,16 @@ criterion = nn.CrossEntropyLoss()
 
 # Training
 def train(epoch):
-    learning_rate = args.lr-(epoch*(args.lr/end_epoch))
-    print(args.lr,epoch,end_epoch,learning_rate)
-    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4, nesterov=False)
-    print('\nEpoch: %d lr=%.3f' % (epoch,learning_rate))
+    print('\nEpoch: %d' % (epoch))
     net.train()
     train_loss = 0
     correct = 0
     total = 0
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
-    print(trainloader)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)#**
 
     for batch_idx, (inputs, targets) in enumerate(trainloader):
+        learning_rate = args.lr - (batch_idx*(0.1/128))
+        optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9, nesterov=False)# weight_decay=5e-4
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
