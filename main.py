640c7764404e19f1c2df8d9da3342e540e27e8ea
TerusLimsurut <few2012few@gmail.com>
2019-08-09 15:30:54 +0900

    train_test

--- main.py
+++ main.py
@@ -159,6 +159,7 @@ def train_one_epoch(model, dataloader):
             learning_rate=0.001
         elif batch_idx>4:
             learning_rate=0.01
+        else:learning_rate=args.lr
         optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=args.momentum, nesterov=False)# weight_decay=5e-4
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
